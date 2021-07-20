a578942b950a046266448a586a699649a46d5454
jungfer <meikjungfer@hotmail.de>
2021-07-20 09:58:43 +0200

    added site-map feature to see possible links at / and /site-map

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from NFDB import nfdb
 
 dbfile = "C:/ProgramData/NeoFly/common.db"
 db = nfdb()
 app = Flask(__name__)
 
+def has_no_empty_params(rule):
+	defaults = rule.defaults if rule.defaults is not None else ()
+	arguments = rule.arguments if rule.arguments is not None else ()
+	return len(defaults) >= len(arguments)
+
 @app.route('/', methods=['GET'])
 def home():
-	return "Hello, World!"
+	return site_map()
+
+@app.route("/site-map", methods=['GET'])
+def site_map():
+	links = []
+	for rule in app.url_map.iter_rules():
+		if "GET" in rule.methods and has_no_empty_params(rule):
+			url = url_for(rule.endpoint, **(rule.defaults or {}))
+			links.append(url)
+	return jsonify(links)
 
 @app.route('/query/<string:sql>', methods=['GET'])
 def query(sql):
